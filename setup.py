140ff8ae1acd7e43b4d6a9ca86a69645ac41bd46
Erik Jonsson Thorén <thecopy@gmail.com>
2014-06-02 11:55:49 +0200

    Added console entry point it setup script

--- setup.py
+++ setup.py
@@ -2,12 +2,17 @@ from distutils.core import setup
 setup(
   name = 'apiary2postman',
   packages = ['apiary2postman'], # this must be the same as the name above
-  version = '0.3',
+  version = '0.4',
   description = 'A tool for converting Blueman API markup from Apiary.io to Postman collection/dumps',
   author = 'Erik Jonsson Thoren',
   author_email = 'thecopy@gmail.com',
   url = 'https://github.com/thecopy/apiary2postman', # use the URL to the github repo
-  download_url = 'https://github.com/thecopy/apiary2postman/tarball/0.3', # I'll explain this in a second
+  download_url = 'https://github.com/thecopy/apiary2postman/tarball/0.4', # I'll explain this in a second
   keywords = ['apiary', 'blueman', 'postman'], # arbitrary keywords
   classifiers = [],
+  entry_points={
+        'console_scripts': [
+            'apiary2postman = apiary2postman.apiary2postman:main',
+        ]
+    }
 )
