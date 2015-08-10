8d2a9c92f3cc631023e3551ab0632125e9dcd202
Erik Jonsson Thoren <thecopy@gmail.com>
2015-08-10 07:26:02 +0200

    Fixes issue #9

--- apiary2postman/converter.py
+++ apiary2postman/converter.py
@@ -24,6 +24,7 @@ def _buildCollectionResponse(apiary):
 	}
 
 	for collection in collections:
+		result['id'] = collection['id']
 		result['folders'] += collection['folders']
 		result['requests'] += collection['requests']
 
