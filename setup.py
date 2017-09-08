c9bdbbac38eb6713d2e27d90c9ea3fe1bcc239fb
Erik Jonsson Thoren <thecopy@gmail.com>
2017-09-08 14:12:42 +0200

    Updated version to 0.5.2

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'apiary2postman',
   packages = ['apiary2postman'], # this must be the same as the name above
-  version = '0.5.1',
+  version = '0.5.2',
   description = 'A tool for converting Blueman API markup from Apiary.io to Postman collection/dumps',
   author = 'Erik Jonsson Thoren',
   author_email = 'thecopy@gmail.com',
   url = 'https://github.com/thecopy/apiary2postman', # use the URL to the github repo
-  download_url = 'https://github.com/thecopy/apiary2postman/tarball/0.5.1', 
+  download_url = 'https://github.com/thecopy/apiary2postman/tarball/0.5.2', 
   keywords = ['apiary', 'blueman', 'postman'], # arbitrary keywords
   classifiers = [],
   entry_points={
